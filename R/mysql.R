7b3b521f21e6e8d16ed644f8da88e7e350ebf3ae
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2022-11-11 10:30:42 -0500

    ALL db calls are now made through a home-made funciton `db_query`. The advantage is that we can more easily control connections to the database, and reduce costs. All connections are short-lived and temporary. The calls are inside a tryCatch making sure we disconnect from the database if there is an error.

--- R/mysql.R
+++ R/mysql.R
@@ -18,6 +18,56 @@ db_connect <- function() {
     dbname = "ccdb")
 }
 
+#' Execute, write or get queries from the MySQL database
+#'
+#' Execute, write or get queries with connection and disconnection included, to
+#' ensure that connections are as short-lived as possible, and that no
+#' connection remains open indefinitely.
+#'
+#' @param type <`character`> One of `execute`, `write` or `get`. They correspond
+#' to `DBI::dbExecute`, `DBI::dbWriteTable` and `DBI::dbGetQuery`.
+#' @param statement <`character` or `data.frame`> A character string containing SQL
+#' for type `execute` and `get`, but a data.frame for type `write`.
+#' @param name <`character`> The table name, mandatory for type `write`. In other
+#' cases, won't be used.
+#'
+#' @return An error or nothing if ran successfully.
+#' @export
+db_query <- function(type, statement, name = NULL) {
+
+  # Connect to database
+  conn <- db_connect()
+
+  # Englobe the call into a tryCatch to disconnect if it fails
+  tryCatch({
+    call_fun <- (\(x) if (x == "execute") return("DBI::dbExecute") else
+      if (x == "write") return("DBI::dbWriteTable") else
+        if (x == "get") return("DBI::dbGetQuery") else
+          return(stop("Argument `type` unrecognized.")))(type)
+
+    if (type == "write" && is.null(name))
+      stop("A name must be given to the data.frame")
+
+    call <- list(conn = conn, statement = statement)
+
+    if (type == "write") {
+      call <- c(call, list(name = name, overwrite = TRUE))
+      names(call)[names(call) == "statement"] <- "value"
+    }
+
+    do.call(eval(parse(text = call_fun)), call)
+  }, error = function(e) {
+    db_disconnect(conn)
+    stop(e)
+  })
+
+  # Disconnect
+  db_disconnect(conn)
+
+  # Return
+  return(invisible(NULL))
+}
+
 #' Disconnect from the AWS MySQL database
 #'
 #' @param conn A \code{\link[DBI]{DBIConnection-class}} object, as returned by
@@ -30,17 +80,15 @@ db_disconnect <- function(conn) {
 
 #' Write the list of processed census data to the MySQL database
 #'
-#' @param conn A \code{\link[DBI]{DBIConnection-class}} object, as returned by
-#' \code{\link[cc.data]{db_connect}}.
 #' @param processed_census_data <`named list`> The finalized process data
 #' normally coming out of the final processing function:
 #' \code{\link[cc.data]{census_reduce_years}}
 #'
-#' @return Returns an error or a success message. All tables in the list fed to
-#' `processed_census_data` are written to the MySQL database with the spatial
-#' features dropped.
+#' @return Returns an error or nothing if ran successfully. All tables in the
+#' list fed to `processed_census_data` are written to the MySQL database with
+#' the spatial features dropped.
 #' @export
-db_write_processed_data <- function(conn, processed_census_data) {
+db_write_processed_data <- function(processed_census_data) {
 
   out <-
     sapply(names(processed_census_data), \(scale) {
@@ -50,36 +98,36 @@ db_write_processed_data <- function(conn, processed_census_data) {
       if ("sf" %in% class(tb)) tb <- sf::st_drop_geometry(tb)
 
       # Write the table
-      DBI::dbWriteTable(conn, tb_name, tb, overwrite = TRUE)
+      db_query(type = "write", statement = tb, name = tb_name)
 
       # Create an index on ID for faster retrieval
       # Modify the column first so that it's character type
       max_cell_size <- max(nchar(tb$ID))
-      DBI::dbExecute(conn, paste("ALTER TABLE", tb_name, "MODIFY COLUMN",
+      db_query(type = "execute",
+               statement = paste("ALTER TABLE", tb_name, "MODIFY COLUMN",
                                  "ID", "VARCHAR(", max_cell_size, ")"))
       # Index the ID column
       index_name <- paste0("ID_index_", tb_name)
-      DBI::dbExecute(conn, paste("CREATE INDEX", index_name,
+      db_query(type = "execute",
+               statement = paste("CREATE INDEX", index_name,
                                  "ON", tb_name, "(ID)"))
     })
 
-  return(message(paste0("`", sum(out), "` tables added successfully.")))
+  return(invisible(NULL))
 }
 
 #' Write the list of scales and years of raw census data to the MySQL database
 #'
-#' @param conn A \code{\link[DBI]{DBIConnection-class}} object, as returned by
-#' \code{\link[cc.data]{db_connect}}.
 #' @param DA_data_raw <`list of sf data.frame`> The DA output of
 #' \code{\link[cc.data]{census_data_raw}}. The DA output of the raw data retrieval.
 #'
-#' @return Returns an error or a success message. All DA tables in the list fed
+#' @return Returns an error or nothing if ran successfully. All DA tables in the list fed
 #' to `DA_data_raw` are written to the MySQL database with the spatial
 #' features transformed to binary then hexadecimal encoded character. If the
 #' character is too large for the MySQL cell limit, the geometry is split in
 #' multiple columns, e.g. geometry_1, geometry_2, etc.
 #' @export
-db_write_raw_data <- function(conn, DA_data_raw) {
+db_write_raw_data <- function(DA_data_raw) {
 
   # The innodb_strict_mode setting affects the handling of syntax errors
   # for CREATE TABLE, ALTER TABLE and CREATE INDEX statements.
@@ -92,9 +140,9 @@ db_write_raw_data <- function(conn, DA_data_raw) {
   # Set it to OFF:
   # `DBI::dbGetQuery(conn, "set innodb_strict_mode = 0;")`
   innodb_strict_mode <-
-    DBI::dbGetQuery(conn, "show variables like '%strict%'")$Value
+    db_query(type = "get", "show variables like '%strict%'")$Value
   if (innodb_strict_mode == "ON")
-    DBI::dbGetQuery(conn, "set innodb_strict_mode = 0;")
+    db_query(type = "execute", "set innodb_strict_mode = 0;")
 
   # Progress bar
   pb <- progressr::progressor(steps = length(DA_data_raw))
@@ -176,30 +224,28 @@ db_write_raw_data <- function(conn, DA_data_raw) {
     #     DBI::dbWriteTable(conn, new_tb_name, tb, overwrite = TRUE)
     #   })
     # } else {
-    DBI::dbWriteTable(conn, tb_name, df_no_geo, overwrite = TRUE)
+    db_query(type = "write", name = tb_name, statement = df_no_geo)
     # }
 
     # Create an index on ID for faster retrieval
     # Modify the column first so that it's character type
     max_cell_size <- max(nchar(df_no_geo$ID))
-    DBI::dbExecute(conn, paste("ALTER TABLE", tb_name, "MODIFY COLUMN",
-                               "ID", "VARCHAR(", max_cell_size, ")"))
+    db_query(type = "execute", paste("ALTER TABLE", tb_name, "MODIFY COLUMN",
+                                     "ID", "VARCHAR(", max_cell_size, ")"))
     # Index the ID column
     index_name <- paste0("ID_index_", tb_name)
-    DBI::dbExecute(conn, paste("CREATE INDEX", index_name,
-                               "ON", tb_name, "(ID)"))
+    db_query(type = "execute", paste("CREATE INDEX", index_name,
+                                     "ON", tb_name, "(ID)"))
 
   }, DA_data_raw, names(DA_data_raw))
 
-  return(message("Success"))
+  return(invisible(NULL))
 }
 
 #' Read a table in the MySQL database
 #'
 #' Read a table in the database by filtering the ID column.
 #'
-#' @param conn A \code{\link[DBI]{DBIConnection-class}} object, as returned by
-#' \code{\link[cc.data]{db_connect}}.
 #' @param table <`character`> The table name in the MySQL database. To list all
 #' tables, use \code{\link[DBI]{dbListTables}}.
 #' @param columns <`character vector`> A character vector of all columns to
@@ -208,13 +254,17 @@ db_write_raw_data <- function(conn, DA_data_raw) {
 #'
 #' @return A tibble or an sf tibble depending if geometry is available.
 #' @export
-db_read_data <- function(conn, table, columns = "*", IDs) {
+db_read_data <- function(table, columns = "*", IDs) {
   IDs_condition <- paste0("ID = ", IDs)
-  query <- paste("SELECT", paste0(columns, collapse = ", ", "FROM"),
+
+  # If not retrieve all columns, make sure to retrieve IDs
+  if (columns != "*") columns <- c("ID", columns, "geometry_*")
+
+  query <- paste("SELECT", paste0(columns, collapse = ", "), "FROM",
                  table, "WHERE",
                  paste0(IDs_condition, collapse = " OR "))
 
-  df <- DBI::dbGetQuery(conn, query)
+  df <- db_query(type = "execute", statement = query)
   df <- df[, names(df) != "row_names"]
   df <- tibble::as_tibble(df)
 
@@ -243,45 +293,41 @@ db_read_data <- function(conn, table, columns = "*", IDs) {
 
 #' Create a read only user to the AWS MySQL database
 #'
-#' @param conn A \code{\link[DBI]{DBIConnection-class}} object, as returned by
-#' \code{\link[cc.data]{db_connect}}.
 #' @param user <`character`> User identifier for the new user
 #' @param password <`character`> Password identifier for the new user
 #' @param admin <`logical`> If the user created should be granted all powers
 #' on the MySQL db. If FALSE, read only permissions are attributed.
 #'
-#' @return Returns an error or a success message. User is then created and
-#' has SELECT and SHOW VIEW privileges only.
+#' @return Returns an error or nothing if ran successfully. User is then created and
+#' has SELECT and SHOW VIEW privileges only if `admin` is set to FALSE (default).
 #' @export
-db_create_user <- function(conn, user, password, admin = FALSE) {
+db_create_user <- function(user, password, admin = FALSE) {
   create <-
-    DBI::dbExecute(conn, sprintf("CREATE USER %s IDENTIFIED BY '%s';",
-                                 user, password))
+    db_query(type = "execute", sprintf("CREATE USER %s IDENTIFIED BY '%s';",
+                                       user, password))
 
   rights <- if (admin) "ALL PRIVILEGES" else "SELECT, SHOW VIEW"
 
   grant <-
-    DBI::dbExecute(conn, sprintf("GRANT %s ON ccdb.* TO %s@'%%';",
-                                 rights, user))
+    db_query(type = "execute", sprintf("GRANT %s ON ccdb.* TO %s@'%%';",
+                                       rights, user))
   flush <-
-    DBI::dbExecute(conn, "FLUSH PRIVILEGES;")
+    db_query(type = "execute", "FLUSH PRIVILEGES;")
 
-  if (sum(create, grant, flush) == 0) message("Success") else stop("Failed")
+  return(invisible(NULL))
 }
 
 #' Delete a user that has access to the AWS MySQL database
 #'
-#' @param conn A \code{\link[DBI]{DBIConnection-class}} object, as returned by
-#' \code{\link[cc.data]{db_connect}}.
 #' @param user <`character`> User identifier for the user to remove
 #'
-#' @return Returns an error or a success message. User's access are then removed,
-#' the user is deleted.
+#' @return Returns an error or nothing if ran successfully. User's access are
+#' then removed and the user is deleted.
 #' @export
-db_delete_user <- function(conn, user) {
-  drop <- DBI::dbExecute(conn, sprintf("DROP USER %s@'%%';", user))
+db_delete_user <- function(user) {
+  drop <- db_query(type = "execute", sprintf("DROP USER %s@'%%';", user))
 
-  if (drop == 0) message("Success") else stop("Failed")
+  return(invisible(NULL))
 }
 
 
