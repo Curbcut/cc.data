323f7e633d905c4a883017e7f744d6f966eccde3
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2023-02-13 16:39:17 -0500

    All data updated for 2021 census (part 2)

--- data-raw/census_all_IDs.R
+++ data-raw/census_all_IDs.R
@@ -1,6 +1,12 @@
 ## CRAETE LIST OF ALL CENSUS IDS DICTIONARY ####################################
 
 census_all_ids <-
-  sapply(cc.data::census_scales, \(x) years_reduced[[x]]$ID)
+  sapply(cc.data::census_scales, \(x) cancensus::get_census(
+    dataset = paste0("CA",
+                     cc.data::census_years[length(cc.data::census_years)] %% 100),
+    regions = list(C = "01"),
+    level = x,
+    quiet = TRUE
+  )$GeoUID)
 
 usethis::use_data(census_all_ids, overwrite = TRUE)
