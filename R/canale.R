f12ef17b2a2a0768d4ce440c778f65cc3ef4cdd3
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2023-03-06 12:46:11 -0500

    OSM download previous to the get three ways function

--- R/canale.R
+++ R/canale.R
@@ -5,58 +5,7 @@ build_canale <- function(DA_table, OSM_cache = TRUE) {
 
   # Intersection density measure --------------------------------------------
 
-  # Grab 2021 streets from OSM
-  osm_pbf <- "http://download.geofabrik.de/north-america/canada-210101.osm.pbf"
-  streets <- osmextract::oe_read(osm_pbf, layer = "lines",
-                                 quiet = FALSE, force_download = !OSM_cache,
-                                 max_file_size = Inf)
-
-  # Retain footbpaths and exclude limited-access highways and their entrances and
-  # exits.
-  fclasses <- c('trunk', 'primary', 'primary', 'secondary', 'tertiary',
-                'unclassified', 'residential', 'pedestrian', 'living_street',
-                'footway', 'steps', 'path')
-  streets <- streets[streets$highway %in% fclasses, ]
-  # Transform streets to the Stats Can CRS
-  streets <- sf::st_transform(streets, 3347)
-
-  # Make a grid on all Canada to separate the work charge to different cores
-  grid <- sf::st_make_grid(streets, n = c(500, 200))
-  grid <- cbind(tibble::tibble(ID = seq_along(grid)), grid)
-  grid <- sf::st_as_sf(tibble::as_tibble(grid))
-  grid <- sf::st_filter(grid, streets)
-
-  # Get all the three way intersections in all the grids
-  progressr::with_progress({
-    pb <- progressr::progressor(nrow(grid))
-    three_ways <- future.apply::future_lapply(seq_len(nrow(grid)), \(x) {
-      df <- sf:::`[.sf`(grid, x, )
-      out <- nngeo::st_nn(df, streets, k = nrow(streets),
-                          maxdist = 20000, progress = FALSE) |>
-        suppressMessages()
-      out <- sf:::`[.sf`(streets, out[[1]], )
-      if (nrow(out) == 0) return(NULL)
-      out <- sf::st_intersection(grid[1,], out)
-      if (nrow(out) == 0) return(NULL)
-      out <- sf::st_intersection(out)
-      out <- out[sf::st_is(out, type = "POINT"), ]
-      pb()
-      unique(out["ID"])
-    })
-  })
-
-  # Get all the three way intersections in streets that touch the lines of the
-  # grid and that we potentially missed in the first step
-  grid_lines <- sf::st_cast(grid, "MULTILINESTRING")
-  streets_on_lines <- sf::st_intersection(grid_lines, streets)
-  streets_on_lines <- sf::st_intersection(streets_on_lines)
-  streets_on_lines <-
-    streets_on_lines[sf::st_is(streets_on_lines, type = "POINT"), ]
-  streets_on_lines <- unique(streets_on_lines["ID"])
-
-  # Bind all three+ way intersections
-  three_ways <- rbind(Reduce(rbind, three_ways), streets_on_lines)
-
+  three_ways <- get_all_three_plus_ways(OSM_cache = OSM_cache)
 
   # Create a buffer of 1km around DAs
   DA_list <- suppressWarnings(split(DA_table, 1:640))
@@ -125,12 +74,12 @@ build_canale <- function(DA_table, OSM_cache = TRUE) {
 #' @return A data frame containing all the three-plus way intersections in
 #' Canada.
 #' @export
-get_all_three_plus_ways <- function(OSM_cache = TRUE) {
+get_all_three_plus_ways <- function(streets, OSM_cache = TRUE) {
   # Grab 2021 streets from OSM
-  osm_pbf <- "http://download.geofabrik.de/north-america/canada-210101.osm.pbf"
-  streets <- osmextract::oe_read(osm_pbf, layer = "lines",
-                                 quiet = FALSE, force_download = !OSM_cache,
-                                 max_file_size = Inf)
+  #osm_pbf <- "http://download.geofabrik.de/north-america/canada-210101.osm.pbf"
+  #streets <- osmextract::oe_read(osm_pbf, layer = "lines",
+   #                              quiet = FALSE, force_download = !OSM_cache,
+  #                               max_file_size = Inf)
 
   # Retain footbpaths and exclude limited-access highways and their entrances and
   # exits.
