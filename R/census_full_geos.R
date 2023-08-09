52704520177e40720e9180ac77949122db744a96
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2023-08-09 14:09:18 -0400

    Split functions for merging full geospatial info (with water)

--- R/census_full_geos.R
+++ R/census_full_geos.R
@@ -1,3 +1,31 @@
+#' Retrieve and merge full census geospatial data with census df
+#'
+#' This function reads the geospatial data from a specified bucket and
+#' merges it with the given data frame of census information.
+#' The geospatial data is identified by the scale name, and it is returned
+#' as a spatial data frame.
+#'
+#' @param df <`data.frame`> The data frame containing the census data.
+#' @param scale_name <`character`> The name of the scale used to identify
+#'   the corresponding census data ("CSD", "CT", ...)..
+#'
+#' @return A spatial data frame containing the merged census and geospatial
+#' data for a particular scale.
+#' @export
+census_switch_full_geo <- function(df, scale_name) {
+  # From the bucket, grab the geometries of the scale
+  new_geos <- bucket_read_object_zip_shp(object = sprintf("%s_shp.zip", scale_name),
+                                         bucket = "curbcut.rawdata")
+  new_geos <- new_geos[1]
+  ID <- names(new_geos)[1]
+
+  # Get the current df and merge the new geometries
+  df <- sf::st_drop_geometry(df)
+  df <- merge(df, new_geos, by.x = "ID", by.y = ID) |>
+    tibble::as_tibble() |>
+    sf::st_as_sf()
+}
+
 #' Append Geospatial Data to Processed Census Data
 #'
 #' This function retrieves the geospatial data for each scale in the
@@ -16,25 +44,10 @@ census_full_geos <- function(processed_census) {
   scales <- seq_along(processed_census)
 
   # Iterate over all of them
-  geos_appended <- future.apply::future_lapply(scales, \(n) {
-    # Get the scale name
-    scale_name <- names(processed_census)[n]
-
-    # From the bucket, grab the geometries of the scale
-    new_geos <- bucket_read_object_zip_shp(object = sprintf("%s_shp.zip", scale_name),
-                                           bucket = "curbcut.rawdata")
-    new_geos <- new_geos[1]
-    ID <- names(new_geos)[1]
-
-    # Get the current df and merge the new geometries
-    df <- processed_census[[n]]
-    df <- sf::st_drop_geometry(df)
-    df <- merge(df, new_geos, by.x = "ID", by.y = ID) |>
-      tibble::as_tibble() |>
-      sf::st_as_sf()
-
-    return(df)
-  }, future.seed = NULL)
+  geos_appended <- lapply(scales, \(n) {
+    census_switch_full_geo(df = processed_census[[n]],
+                           scale_name = names(processed_census)[[n]])
+  })
 
   # Use the same name as previously
   names(geos_appended) <- names(processed_census)
